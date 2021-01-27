7b9a3a9135d214c4c4501f72cfa389247424ef45
dangthithoait20a3b <77031489+dangthithoait20a3b@users.noreply.github.com>
2021-01-27 22:46:28 +0700

    Add files via upload

--- cau1.java
+++ cau1.java
@@ -1,26 +1,22 @@
-package baitapontapcuoinam;
+package giuakylan2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class cau1 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 10; i < 201; i++) {
-            if ((i % 7 == 0) && (i % 5 != 0)) {
-                list.add(i);
-            }
-        }
-        showList(list);
-    }
-
-    public static void showList(List<Integer> list) {
-        if (list != null && !list.isEmpty()) {
-            int size = list.size();
-            for (int i = 0; i < size - 1; i++) {
-                System.out.print(list.get(i) + ", ");
-            }
-            System.out.println(list.get(size - 1));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập x: ");
+        int x = scanner.nextInt();
+        System.out.println("Nhập y: ");
+        int y = scanner.nextInt();
+        if(x>3 && y>3){
+            System.out.println("Trước khi hoán vị " + x + "\t" +y);
+            int c = x;
+            x=y;
+            y=c;
+            System.out.println("Sau khi hoán vị " + x +"\t" +y);
+        }else{
+            System.out.println(" nhập lại: Điều kiện sai");
         }
     }
 }
--- cau3.java
+++ cau3.java
@@ -1,22 +1,44 @@
-package baitapontapcuoinam;
+package giuakylan2;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class cau3 {
-    private static Scanner scanner = new Scanner(System. in );
-
+    //1.1
     public static void main(String[] args) {
-        System.out.print("Nhập số nguyên dương n = ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập n: ");
         int n = scanner.nextInt();
-
-        Map < Integer,
-                Integer > map = new HashMap < Integer,
-                Integer > ();
-        for (int i = 1; i < n + 1; i++) {
-            map.put(i, i * i);
+        int A[] = new int[n];
+        for (int i = 1; i < n; i++) {
+            System.out.println("Phần tử thứ " + i + ": ");
+            A[i] = scanner.nextInt();
         }
-        System.out.println(map);
+        System.out.println("Mảng vừa nhập là: ");
+        for (int i = 1; i < n; i++) {
+            System.out.println(A[i] + "\t");
+        }
+        //3.1
+        int max = A[0];
+        for (int i = 0; i < n; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        System.out.println("Giá trị lớn nhất trong mảng là: " + max);
+        int max1 = A[0];
+        int max2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i] > max2) {
+                if (A[i] > max1) {
+                    max2 = max1;
+                    max1 = A[i];
+                } else
+                    max2 = A[i];
+            } else if (max2 >= max1)
+                max2 = A[i];
+        }
+        System.out.println("Giá trị lớn thứ 2 trong mảng là: " + max2);
+        //3.2
+        //3.3
     }
 }
